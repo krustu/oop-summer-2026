12987c33b9788a7bdf07ecd531dcdb353fb4e153
krustu <127596193+krustu@users.noreply.github.com>
2026-03-12 10:15:38 +0000

    tes2done

---
+++ STUDENT 35691/Lecture_2_entry_test.py
@@ -0,0 +1,15 @@
+# integer variable
+My_birthday_in = 26
+print(My_birthday_in)
+print(type(My_birthday_in))
+#string var
+Full_name = 'Rysbek Dokturbaev'
+print (Full_name)
+print (type(Full_name))
+#float var
+MyGrade = 4.5
+#convert to integer
+MyGrade_int = int(MyGrade)
+
+print(MyGrade_int)
+print(type(MyGrade_int))
---
+++ STUDENT 35691/test2.py
@@ -0,0 +1,27 @@
+name = "krustu"
+x = "hello"
+y = 4.2
+y = y - 1.3
+my_name_is = 'SHNkrustu'
+krustu = {
+    "name" : "krustru",
+    "age" : 18,
+    
+}
+
+print (x + " " + name)
+print (my_name_is)
+print (y)
+print(str(int(y)))
+print(type(y))
+print(krustu)
+
+
+
+
+
+
+# str = hello world = string
+# float number with point
+# my_name_is = 'KRustu'
+#print x, name
