6fc052f5c8936939487223dce7c3aa485c16cc32
krustu <127596193+krustu@users.noreply.github.com>
2026-04-09 08:36:29 +0000

    Lecture Lab 6

---
+++ STUDENT 35691/test_06_class_with_constructor.py
@@ -0,0 +1,20 @@
+class Car:
+    def__init__(self, name, colour, size):
+        self.name = name 
+        self.colour = colour
+        self.size = size
+
+a1 = Car("Bmw", "black", "Small")
+a2 = Car("Mercedes", "Purple", "Small")
+a3 = Car("toyota", "Red", "Large")
+print(a1.name)
+print(a1.colour)
+print(a1.size)
+
+print(a2.name)
+print(a2.colour)
+print(a2.size)
+
+print(a3.name)
+print(a3.colour)
+print(a3.size)
---
+++ STUDENT 35691/test_06_empty_class.py
@@ -0,0 +1,11 @@
+class Car:
+    pass
+
+ a = Car()
+ a.name = "BMW"
+ a.colour = "black"
+ a.size = "Small"
+
+print(a.name)
+print(a.colour)
+print(a.size)
