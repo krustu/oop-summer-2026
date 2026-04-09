3e2415c2412c7e0506b9cee5db47dff82260a84d
krustu <127596193+krustu@users.noreply.github.com>
2026-04-09 08:00:57 +0000

    Lecture6

---
+++ STUDENT 35691/lecture_6_advance_task.py
@@ -0,0 +1,10 @@
+class Dog:
+    def speak(self):
+        return "Gav"
+
+class Cat:
+    def speak(self):
+        return "Meow"
+        pets = [Dog(), Ca()]
+        for a in pets:
+            print(a.speak( ))
